e78d84a5520d3c519c5535afcb87d9660638a8ea
Theodoro Mota <theolm.mota@gmail.com>
2024-03-25 17:57:03 -0300

    Adding publish plugin

--- file-logger/build.gradle.kts
+++ file-logger/build.gradle.kts
@@ -3,7 +3,7 @@ import config.Config
 
 plugins {
     id("kmm-module-setup")
-    id("com.vanniktech.maven.publish")
+    id("publish-setup")
 }
 
 val artifactId = "txtlogwriter"
@@ -21,34 +21,12 @@ kotlin {
 }
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.DEFAULT)
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
-
     coordinates(Config.groupId, artifactId, Config.libVersion)
 
     pom {
         name.set("Txt Log Writer")
         description.set("It's a simple Kermit log writer that writes logs to a text file.")
-        inceptionYear.set("2024")
-        url.set("https://github.com/theolm/kermit-extensions")
-        licenses {
-            license {
-                name.set("WTFPL - Do What The F*ck You Want To Public License, Version 2.0")
-                url.set("http://www.wtfpl.net/")
-                distribution.set("http://www.wtfpl.net/")
-            }
-        }
-        developers {
-            developer {
-                id.set("theolm")
-                name.set("Theodoro Loureiro Mota")
-                url.set("https://github.com/theolm/")
-            }
-        }
-        scm {
-            url.set("https://github.com/theolm/kermit-extensions/")
-            connection.set("scm:git:git://github.com/theolm/kermit-extensions.git")
-            developerConnection.set("scm:git:ssh://git@github.com/theolm/kermit-extensions.git")
-        }
     }
 }
